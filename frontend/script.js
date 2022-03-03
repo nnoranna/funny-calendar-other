aaad05dc31a5d6ebc9610e055ca2eb9d3c95b423
nnoranna <anagy.nora.anna@gmail.com>
2022-03-03 15:11:57 +0100

    Practice callback function

--- frontend/script.js
+++ frontend/script.js
@@ -1,75 +1,33 @@
-function Country(name, short, population, flag, continent) {
-    this.name = name;
-    this.short = short;
-    this.population = population;
-    this.flag = flag;
-    this.continent = continent;
-}
-
-//Components
-const menuButton = _ => {
-    return `
-    <button id="menuButton">
-        <svg width="40" height="40">
-            <rect width="20" height="2" />
-            <rect width="20" height="2" />
-            <rect width="20" height="2" />
-        </svg>
-        <span>button</span>
-    </button>
-    `;
-}
-
-const header = (logo, button) => {
-    return `
-    <header>
-        <a id = "logo">${logo}</a>
-        ${button()}
-    </header>
-    `;
-}
-
-const countryCard = (name, short, population, flag, continent) => {
-    return `
-    <div class="card">
-        <h1>${name}</h1>
-        <h3>${short}</h3>
-        <p>${population}</p>
-        <img src="${flag}"></img>
-        <p>${continent}</p>
-    </div>
-    `
-}
-
-
-const loadEvent = async _ => {
-    //get data
-    /*await async művelet, hogy megvárjuk a letöltődést; azért kell kétszer, hogy megvárjuk a fetch-et, aztán json-ná formázás*/
-    const countryRes = await fetch("https://restcountries.com/v3.1/all");
-    const countryArr = await countryRes.json();
-    //console.log(countryArr[0]);
-
-    //Process data
-    let countries = countryArr.map(function (country) {
-        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0])
-    })
-    console.log(countries);
-
-    //Render components
-    const rootElement = document.getElementById("root")
-    rootElement.insertAdjacentHTML("beforeend", 
-    header("Countries", menuButton));
+function init(mathFunction) {
+    var a = 5;
+    var b = 6;
+    //console.log(c);
     
-    let cards = "";
-    for (const country of countries) {
-        cards += countryCard(country.name, country.short, country.population, country.flag, country.continent)
+    if (a < b){
+        let c = mathFunction(b, a);
+        /*(function(){
+            var c = b - a;
+        })();*/
+        console.log(c);
+    } 
+    else {
+        let c = mathFunction(a, b)
+        /*(function(){
+            var c = a - b;
+        })();  */
+        console.log(c);
     }
-    rootElement.insertAdjacentHTML("beforeend", cards);
 
-    const getMenuButton = document.getElementById("menuButton");
-    getMenuButton.addEventListener("click", (event) => {
-        event.target.classList.toggle("clicked")
-    });
+    //console.log(c);
 }
 
-window.addEventListener("load", loadEvent)    
+const initC = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+};
+
+const initD = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+
+
+init(initD);
